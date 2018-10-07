b199aa021a15df673eff578c7aa2f4434c71e82a
simona.domnisoru <simona.domnisoru@gmail.com>
2018-10-07 12:45:25 +0200

    update readme

--- src/state/reducer.js
+++ src/state/reducer.js
@@ -2,14 +2,14 @@ import actionTypes from './actions';
 
 const reducerTransactions = (state, action) => {
     switch (action.type) {
-        case actionTypes.send:
-            const sent = state.sent + action.transaction.amount;
-            const left = state.left - action.transaction.amount;
-            const transaction = { ...action.transaction, id: state.transactions.length };
-            const percentage = (100 * left)/20000;
-            return { ...state, transactions: [...state.transactions, transaction], left, sent, percentage };
-        default:
-            return state;
+    case actionTypes.send:
+        const sent = state.sent + action.transaction.amount;
+        const left = state.left - action.transaction.amount;
+        const transaction = { ...action.transaction, id: state.transactions.length };
+        const percentage = (100 * left)/20000;
+        return { ...state, transactions: [...state.transactions, transaction], left, sent, percentage };
+    default:
+        return state;
     }
 };
 export default reducerTransactions;
